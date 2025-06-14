b653062cd59efc5c62d2ba101f3151b72f85d599
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-06-14 22:58:12 +0600

    removing old code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:quran_app/features/home_old/presentation/screens/home_screen.dart';
-
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'features/home/presentation/screens/home_screen.dart';
 
-import 'features/home/view/home_screen.dart';
 
 
 void main() async {
