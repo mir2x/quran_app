f0c75ef3afb4dd1510337335fd225109e7c519f5
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-06-16 10:49:45 +0600

    removing old quran code

--- lib/features/quran_old/presentation/providers/bookmark/bookmark_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'bookmark.dart';
-import 'bookmark_notifier.dart';
-
-final bookmarkProvider =
-AsyncNotifierProvider<BookmarkNotifier, List<Bookmark>>(BookmarkNotifier.new);
--- lib/features/quran_old/presentation/providers/quran_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:quran_app/features/quran_old/presentation/providers/quran_notifiers.dart';
-import 'package:quran_app/features/quran_old/presentation/providers/quran_state.dart';
-
-
-final quranProvider =
-StateNotifierProvider<QuranNotifier, QuranState>((ref) => QuranNotifier());
